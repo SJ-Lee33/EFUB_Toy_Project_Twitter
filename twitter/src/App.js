1a5b5254ebcb81831f81f43e0ffec23c3d51e93e
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-05-04 22:41:07 +0900

    feat : add Menu.js 메뉴 컴포넌트 생성

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div>
       <Home />
+      <Menu />
     </div>
   );
 }
---
+++ twitter/src/components/Menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <main>Menu</main>;
+};
+
+export default Menu;
