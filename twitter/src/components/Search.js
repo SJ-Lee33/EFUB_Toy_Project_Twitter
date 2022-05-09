d6885bae099eea698a790357ffae4901e0b1837f
SJ-Lee33 <isjift55@ewhain.net>
2022-05-09 17:14:51 +0900

    feat: add Search UI

---
+++ twitter/src/components/Search.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import { FiSearch } from "react-icons/fi";
+
+export default function Search() {
+  return (
+    <SearchArea>
+      <FiSearch style={{ paddingLeft: "20px", paddingRight: "10px" }} />
+      <form>
+        <SearchBar type="text" name="search" placeholder="트위터 검색" />
+      </form>
+    </SearchArea>
+  );
+}
+
+const SearchArea = styled.div`
+  margin: 10px 0px 10px 20px;
+  background: #f0f3f4;
+  border-radius: 30px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SearchBar = styled.input`
+  width: 100%;
+  border: none;
+  background: transparent;
+
+  overflow: hidden;
+  resize: none;
+  outline: none;
+`;
