8342546b30c27ce38f2842866831e1944d7fc76e
SJ-Lee33 <isjift55@ewhain.net>
2022-05-10 00:10:17 +0900

    fix: modify RecommendBar to fit each page design

--- twitter/src/pages/HomePage.js
+++ twitter/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "../components/Home";
 import MenuBar from "../components/MenuBar";
-import RecommendBar from "../components/RecommendBar";
+import RecommendBar from "../components/RecommendBarH";
 
 export default function HomePage() {
   return (
--- twitter/src/pages/ProfilePage.js
+++ twitter/src/pages/ProfilePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "../components/Home";
 import MenuBar from "../components/MenuBar";
 import Profile from "../components/Profile";
-import RecommendBar from "../components/RecommendBar";
+import RecommendBar from "../components/RecommendBarP";
 
 export default function ProfilePage() {
   return (
