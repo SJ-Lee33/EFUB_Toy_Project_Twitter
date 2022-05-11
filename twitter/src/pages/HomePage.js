2bba04d64ac591519668d6723eb09f237dd2dddb
dayun <81161750+dy6578ekdbs@users.noreply.github.com>
2022-05-11 19:04:37 +0900

    feat : add Loading Spinner component

--- twitter/src/pages/HomePage.js
+++ twitter/src/pages/HomePage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from "../components/Home";
+import Spinner from "../components/Spinner";
 import MenuBar from "../components/MenuBar";
 import RecommendBar from "../components/RecommendBarH";
 
-export default function HomePage() {
+export default function HomePage({ articles, loading }) {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <MenuBar />
 
-      <Home />
+      <Home articles={articles} loading={loading} />
 
       <RecommendBar />
     </div>
